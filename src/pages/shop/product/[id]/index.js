b22677fe6b91e5ec7f075ba3c41c465f04e24e7f
Rymuu <96126158+Rymuu@users.noreply.github.com>
2022-05-23 03:00:26 +0200

    ia

--- src/pages/shop/product/[id]/index.js
+++ src/pages/shop/product/[id]/index.js
@@ -2,26 +2,64 @@ import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import productService from "../../../../services/product.service";
 import ProductCardId from "../../../../components/ProductCardId";
+import RecImageSlider from "../../../../components/RecommandationSlider";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import axios from "axios";
 
 const Index = () => {
   const router = useRouter();
   const [product, setProduct] = useState();
+  const [products, setProducts] = useState();
 
   useEffect(() => {
     const id = router.query.id;
 
     productService.getProduct(id)
       .then((data) => {
-      setProduct(data.data[0]);
-      console.log("the data",data.data[0]);
-    })
-    .catch(err => console.log(err))
-    },[]);
+        setProduct(data.data[0]);
+        console.log("the data", data.data[0]);
+      })
+      .catch(err => console.log(err))
+  }, []);
+
+  useEffect(() => {
+    let formData = new FormData();
+    formData.append("categorie", product && product.categorie.id)
+    const result = axios.post("http://localhost:8000/produit/filtre", formData)
+      .then((res) => {
+        setProducts(res.data.data[0])
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+  }, [product]);
+
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
 
   return (
     <div className="product_page">
-      <ProductCardId 
-      product={product && product} key={product && product.id}/>
+      <ProductCardId
+        product={product && product} key={product && product.id} />
+      <RecImageSlider className="image__slider" products={products} />
     </div>
   );
 };
